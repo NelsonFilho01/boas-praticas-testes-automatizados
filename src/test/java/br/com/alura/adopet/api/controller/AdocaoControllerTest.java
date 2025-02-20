ae47a2c3caa40043d3a04d3c37963806933f487b
NelsonFilho01 <nelsoncamarafilho@gmail.com>
2025-02-20 16:24:33 -0300

    Testes

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,64 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService service;
+
+    @Test
+    void solicitacoesAdocao400() throws Exception {
+
+        String json = "{}";
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        Assertions.assertEquals(400, response.getStatus());
+
+    }
+
+    @Test
+    void solicitacoesAdocao200() throws Exception {
+
+        String json = """
+                {
+                "idPet": 1,
+                   "idTutor": 1,   
+                    "motivo": "Motivo qualquer"
+                    }
+                
+                """;
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        Assertions.assertEquals(200, response.getStatus());
+
+
+    }
+}
